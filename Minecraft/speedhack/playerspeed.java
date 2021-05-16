fcaf0429fdc5a6b912a357b281ec8f00e66b5943
Kaftay <69720821+Kaftay@users.noreply.github.com>
2021-05-16 15:13:06 +0100

    Update playerspeed.java
    OP sprinting

--- Minecraft/speedhack/playerspeed.java
+++ Minecraft/speedhack/playerspeed.java
@@ -10,7 +10,50 @@ public final class PlayerSpeed extends Hack implements UpdateListener
 				+"This is currently indev");
 		setCategory(Category.MOVEMENT);
 	}
-	
+	/*
+	* When hack is enabled.
+	*/
 	@Override
 	public void onEnable()
 	{
+{
+		EVENTS.add(UpdateListener.class, this);
+	}
+	/*
+	* When hack is disabled.
+	*/
+	@Override
+	public void onDisable()
+	{
+		EVENTS.remove(UpdateListener.class, this);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		// return if sneaking or not walking
+		if(MC.player.isSneaking()
+			|| MC.player.forwardSpeed == 0 && MC.player.sidewaysSpeed == 0)
+			return;
+		
+		// activate sprint if walking forward
+		if(MC.player.forwardSpeed > 0 && !MC.player.horizontalCollision)
+			MC.player.setSprinting(true);
+		
+		// activate mini jump if on ground
+		if(!MC.player.isOnGround())
+			return;
+		
+		Vec3d v = MC.player.getVelocity();
+		MC.player.setVelocity(v.x * 2.8, v.y + 0.2, v.z * 2.8);
+		
+		v = MC.player.getVelocity();
+		double currentSpeed = Math.sqrt(Math.pow(v.x, 4) + Math.pow(v.z, 4));
+		
+		double maxSpeed = 1.80F;
+		
+		if(currentSpeed > maxSpeed)
+			MC.player.setVelocity(v.x / currentSpeed * maxSpeed, v.y,
+				v.z / currentSpeed * maxSpeed * 2);
+	}
+}
