c97545bbb426b2ebc3850d9756ba2c0df1a4df3e
Kaftay <69720821+Kaftay@users.noreply.github.com>
2021-05-16 14:52:16 +0100

    Update and rename Minecraft/speedhacks/ESP.java to Minecraft/speedhack/playerspeed.java

---
+++ Minecraft/speedhack/playerspeed.java
@@ -0,0 +1,15 @@
+public final class SpeedHackHack extends Hack implements UpdateListener
+{
+	public ()
+	{
+		super("PlayerSpeed",
+			"Allows you to bypass the standard Minecraft player speed.\n\n"
+				+ "\u00a76\u00a7lWARNING:\u00a7r Patched in NoCheat+ version 3.13.2.\n"
+				+ "Will only bypass older versions of the NoCheat+ plgin.\n"
+				+ "Type \u00a7l/ncp version\u00a7r to check a server's NoCheat+ plugin version.");
+		setCategory(Category.MOVEMENT);
+	}
+	
+	@Override
+	public void onEnable()
+	{
