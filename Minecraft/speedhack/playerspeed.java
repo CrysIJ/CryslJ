0fe8d0f8dfa63e69798e3ac395f57ed699851895
Kaftay <69720821+Kaftay@users.noreply.github.com>
2021-05-16 14:59:05 +0100

    Update playerspeed.java

--- Minecraft/speedhack/playerspeed.java
+++ Minecraft/speedhack/playerspeed.java
@@ -6,7 +6,8 @@ public final class SpeedHackHack extends Hack implements UpdateListener
 			"Allows you to bypass the standard Minecraft player speed.\n\n"
 				+ "\u00a76\u00a7lWARNING:\u00a7r Patched in NoCheat+ version 3.13.2.\n"
 				+ "Will only bypass older versions of the NoCheat+ plgin.\n"
-				+ "Type \u00a7l/ncp version\u00a7r to check a server's NoCheat+ plugin version.");
+				+ "Type \u00a7l/ncp version\u00a7r to check a server's NoCheat+ plugin version.\n"
+				+ "This is currently indev. Do not download until ready");
 		setCategory(Category.MOVEMENT);
 	}
 	
